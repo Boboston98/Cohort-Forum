d720e8798637d41bb8702e38eb4e49a1b996b4d5
David Olivares <david.olvrs@gmail.com>
2017-08-07 16:15:53 -0700

    added 3 main tasks for App Engine API

--- cohort-forum/main.py
+++ cohort-forum/main.py
@@ -17,13 +17,22 @@
 import webapp2
 import os
 import jinja2
-from google.appengine.api import users 
+from google.appengine.api import users
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Hello.")
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or Register</a>.' %
+                users.create_login_url('/'))
+
+        self.response.write('<html><body>%s</body></html>' % greeting)
+
 
 class ChatHandler(webapp2.RequestHandler):
     def get(self):
