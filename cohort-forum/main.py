51824d23f964ee96de4deeffaa089a52e0b348d5
David Olivares <david.olvrs@gmail.com>
2017-08-08 10:10:32 -0700

    ugh

--- cohort-forum/main.py
+++ cohort-forum/main.py
@@ -37,11 +37,9 @@ class ProfileHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/profile.html')
         self.response.write(template.render())
-        profile_dict ={
-
-
-        
+        profile = {"David":"Green", "Austin":"Black","Jose":"Burgundy", "Matthew":"Blue"
         }
+        
 
 class ChatHandler(webapp2.RequestHandler):
     def get(self):
